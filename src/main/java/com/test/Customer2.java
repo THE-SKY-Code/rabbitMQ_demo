62f4b5bdea25a70b6f4b48216102ca1db26e161b
THE-SKY-Code <1422678841@qq.com>
2019-11-20 21:45:34 +0800

    test

--- src/main/java/com/test/Customer2.java
+++ src/main/java/com/test/Customer2.java
@@ -10,5 +10,7 @@ public class Customer2 {
     @RabbitHandler
     public void  show(String mes){
         System.out.println("itheima"+mes);
+        System.out.println("hello");
+        System.out.println("world");
     }
 }
